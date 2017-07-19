4cd474a407a056b13f79f7bd197383b4af5c465b
Yonghui Chen <chyhfj@gmail.com>
2017-07-19 11:14:14 +0800

    tap to show/hide overlay

---
+++ Assets/Scripts/OverlayShowHide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayShowHide : MonoBehaviour {
+
+    public OVROverlay overlay;
+	
+	void Update () {
+        if (OVRInput.GetUp(OVRInput.Button.One)) {
+            overlay.enabled = !overlay.enabled;
+        }
+	}
+}
